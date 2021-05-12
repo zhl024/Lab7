b42e652291612dd711f5f4f9f662a2ddaf3eb424
Camdyn Rasque <rasque.camdyn@gmail.com>
2021-05-12 11:17:48 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
 // sw.js - Service Worker
+
+// You will need 3 event listeners:
+//   - One for installation
+//   - One for activation ( check out MDN's clients.claim() for this step )
+//   - One for fetch requests
