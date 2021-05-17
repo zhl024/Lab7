0d42b826e583c1dbe7ad8781dce20a1e6304c133
Zhongqian Li <64935761+zhl024@users.noreply.github.com>
2021-05-16 21:00:46 -0500

    fix path issue

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const setState = router.setState;
 // Make sure you register your service worker here too
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
